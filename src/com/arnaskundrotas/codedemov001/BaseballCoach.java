9b4844725ffff2e48cb275ec5d9cb8f27ff1c080
Arnas Kundrotas <arnas.kundrotas@gmail.com>
2022-12-09 08:07:26 +0200

    codedemov001 - created object and called method on it

---
+++ src/com/arnaskundrotas/codedemov001/BaseballCoach.java
@@ -0,0 +1,8 @@
+package com.arnaskundrotas.codedemov001;
+
+public class BaseballCoach {
+	
+	public String getDailyWorkOut () {
+		return "Spend 30 minutes on batting practice";
+	}
+}
---
+++ src/com/arnaskundrotas/codedemov001/MyApp.java
@@ -0,0 +1,16 @@
+package com.arnaskundrotas.codedemov001;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+	
+		// create the object
+		
+		BaseballCoach theCoach = new BaseballCoach();
+		
+		
+		// use the object
+		System.out.println(theCoach.getDailyWorkOut());
+	}
+
+}
