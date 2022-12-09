6ee9e92d507a78d8a1ecb2501ae43fadd11566e3
Arnas Kundrotas <arnas.kundrotas@gmail.com>
2022-12-09 08:58:15 +0200

    Added applicationContext.xml

--- src/com/arnaskundrotas/codedemov001/MyApp.java
+++ src/com/arnaskundrotas/codedemov001/MyApp.java
@@ -5,8 +5,7 @@ public class MyApp {
 	public static void main(String[] args) {
 	
 		// create the object
-		
-		Coach theCoach = new BaseballCoach();
+		Coach theCoach = new TrackCoach();
 		
 		
 		// use the object
---
+++ src/com/arnaskundrotas/codedemov001/TrackCoach.java
@@ -0,0 +1,11 @@
+package com.arnaskundrotas.codedemov001;
+
+public class TrackCoach implements Coach {
+
+	@Override
+	public String getDailyWorkOut() {
+		
+		return "Run a had 5k";
+	}
+
+}
